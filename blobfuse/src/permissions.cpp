d9b3c309ca92d443131ab62fca74b4ec8dd17cb8
amanda <amnguye@microsoft.com>
2020-03-29 15:08:10 -0700

    update to permissions

--- blobfuse/src/permissions.cpp
+++ blobfuse/src/permissions.cpp
@@ -5,7 +5,10 @@
 #include <permissions.h>
 
 std::string modeToString(mode_t mode) {
-    std::string result = "user::"; // = "user::rwx,group::r--,mask::rwx,other::---";
+    //The format for the value x-ms-acl is user::rwx,group::rwx,mask::rwx,other::rwx
+    //Since fuse doesn't have a way to expose mask to the user, we only are concerned about
+    // user, group and other.
+    std::string result = "user::";
 
     result.push_back(mode & (1 << 8) ? 'r': '-');
     result.push_back(mode & (1 << 7) ? 'w' : '-');
